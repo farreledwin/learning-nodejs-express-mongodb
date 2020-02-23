dd73225b67674d62cc758350e0906f9d638fb623
farreledwin <farrel206@gmail.com>
2020-02-23 15:04:54 +0700

    testing get method json via file

--- nodejs_with_express/app.js
+++ nodejs_with_express/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		results: tours.length,
+		data: {
+			tours
+		}
+	});
+});
 
 app.get('/', (req, res) => {
 	res.status(200).json({ message: 'Accessing Root URL and testing sending response JSON', app: 'natours' });
